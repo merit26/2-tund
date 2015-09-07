16a5c62e3a2911f7b3cf596876983bbaf3ca18f4
Merit Paist <meripai@greeny.cs.tlu.ee>
2015-09-07 14:57:48 +0300

    vormid

---
+++ user_form.php
@@ -0,0 +1,19 @@
+<?php
+
+	
+
+?>
+<html>
+	<head>
+		<title>User login page</title>
+	</head>
+	<body>
+		<h2>Login</h2>
+		<input type="email" placeholder="E-post"> <br> <br>
+		<input type="password" placeholder="parool"> <br> <br>
+		
+		<input type="submit" value="logi sisse"
+		
+		<h2>Create user</h2>
+	</body>
+</html>
