c5c8328026f84533c9b7f1268e3a98438e47b064
Merit Paist <meripai@greeny.cs.tlu.ee>
2015-09-07 13:47:05 +0300

    loogika

--- helloworld.php
+++ helloworld.php
@@ -1,7 +1,27 @@
 <?php
 
-$first_name = "Merit";
+		$first_name = "Merit";
+		$last_name= "Paist";
 
-echo $first_name; // trükkimine
+		echo $first_name." ".$last_name; // trükkimine
+
+?>
+
+<br>
+
+<?php
+
+	$age = 5;
+	// if (loogikatehe) {tõene} else {väär}
+	if($age < 18){
+		echo "alaealine";
+	} else { 
+		echo "täisealine";
+		
+	}
+
+
+
+
 
 ?>
